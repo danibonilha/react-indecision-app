6f6a60cbd854d8f69b19abc6a0df0c495542d010
danibonilha <dani.bonilha@live.com>
2018-05-02 19:07:46 -0300

    Challenge - Changing build-visible to visibility-toogle with React Components.

---
+++ src/playground/visibility-toogle.js
@@ -0,0 +1,62 @@
+class VisibilityToogle extends React.Component {
+	constructor(props) {
+		super(props)
+		this.onToogle = this.onToogle.bind(this);
+		this.state = {
+			title: 'Visibility Toggle',
+			visibility: false,
+			detailsText: "My details paragraph"
+		}
+	}
+	onToogle() {
+		this.setState((prevState) => {
+			return {
+				visibility: !prevState.visibility
+			};
+		});
+	}
+	render() {
+		return (
+			<div>
+				<Header title={this.state.title} />
+				<Button
+					click={this.onToogle}
+					buttonName={
+						this.state.visibility ? 'Hide Details' : 'Show Details'
+					}
+				/>
+				{this.state.visibility && <Details text={this.state.detailsText} />}
+			</div>
+		);
+	}
+
+}
+class Header extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>{this.props.title}</h1>
+			</div>
+		);
+	}
+}
+class Button extends React.Component {
+	render() {
+		return (
+			<div>
+				<button onClick={this.props.click}>{this.props.buttonName}</button>
+			</div>
+		);
+	}
+}
+class Details extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>{this.props.text}</p>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<VisibilityToogle />, document.getElementById('app'));
